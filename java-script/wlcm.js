438d9d9d4a488a9ead5f82d67a22e180cfce84c8
K Amrutha Sai <128003134@sastra.ac.in>
2026-06-08 18:40:36 +0530

    added notes feature and todo list with local storage

--- java-script/wlcm.js
+++ java-script/wlcm.js
@@ -20,10 +20,12 @@ function showMenu(){
       <button class="menu-btn" onclick="
         showTodo();
       ">
-        notes
-      </button>
-      <button class="menu-btn">
         to-do
+      </button>
+      <button class="menu-btn" onclick="
+        showNotes();
+      ">
+        notes
       </button>
       <button class="menu-btn">
         calculator
@@ -56,17 +58,24 @@ function showTodo(){
           <button class="plus">+</button>
           <span class="txt">click to add a new todo page</span>
         </div>
-  </div>
+    </div>
   `;
+  displayTask();
 }
 
-let taskArr=[];
+let taskArr = JSON.parse(localStorage.getItem('tasks')) || [];
+
 function addTask(){
   let taskInp= document.querySelector('.inp');
   let task=taskInp.value.trim();
+  if(task === "") return;
   taskArr.push(task);
   taskInp.value=' ';
-  console.log(taskArr); 
+  localStorage.setItem('tasks',JSON.stringify(taskArr));
+  displayTask();
+}
+
+function displayTask(){
   let sp=document.querySelector('.todo-list');
   sp.innerHTML = ' ';
   for(let i=0;i<taskArr.length;i++){
@@ -84,6 +93,38 @@ function taskKey(event){
   if(btn==='Enter'){
     addTask();
   }
+}
+
+function showNotes(){
+  app.innerHTML=`
+    <div class="write-space">
+      Write your heart here..
+      <textarea class="write-inp" onkeyup="
+        saveText();
+      "></textarea>
+      <div class="bottom-btns">
+          <div class="btn-wrap">
+            <button class="bplus" onclick="
+              showMenu();
+            ">B</button>
+            <span class="txt">click to go back</span>
+        </div>
+
+          <div class="btn-wrap">
+            <button class="plus">+</button>
+            <span class="txt">click to add a new notes </span>
+          </div>
+      </div>
+    </div>
+  `;
+  let textElm = document.querySelector('.write-inp');
+  textElm.value = localStorage.getItem('notes') || "";
+}
+
+function saveText(){
+  let textElm = document.querySelector('.write-inp');
+  let text = textElm.value;
+  localStorage.setItem('notes', text);
 }
 
 
